0bf5e9ae6964664dfb475c051290552bcf65adde
frebro <frebro@gmail.com>
2013-02-26 12:26:17 +0100

    removed static ga code

--- wordpress/wp-content/themes/devnull/index.php
+++ wordpress/wp-content/themes/devnull/index.php
@@ -72,13 +72,6 @@ get_header(); ?>
     <script src="js/helper.js"></script>
     <script src="js/main.js"></script>
 
-    <script>
-      var _gaq=[["_setAccount","UA-XXXXX-X"],["_trackPageview"]];
-      (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
-      g.src=("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";
-      s.parentNode.insertBefore(g,s)}(document,"script"));
-    </script>
-
   </body>
 
 </html>
