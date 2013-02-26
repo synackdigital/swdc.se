a31e296f969a310c516a5fc813cbb2cf94acb5d8
frebro <frebro@gmail.com>
2013-02-26 10:52:31 +0100

    killed posts, pages and tools in admin menu

--- wordpress/wp-content/themes/devnull/functions.php
+++ wordpress/wp-content/themes/devnull/functions.php
@@ -4,10 +4,17 @@
  * @subpackage devnull
  */
 
+/* Basic theme setup */
 function devnull_setup() {
-
-  // Add post thumbnail support
-  add_theme_support( 'post-thumbnails' );
+  add_theme_support( 'post-thumbnails' ); // Add post thumbnail support
   set_post_thumbnail_size( 200, 200, true ); // hard crop
 }
 add_action( 'after_setup_theme', 'devnull_setup' );
+
+/* Filter out unused admin menu items */
+function devnull_remove_menu_pages() {
+  remove_menu_page('edit.php'); // posts
+  remove_menu_page('edit.php?post_type=page'); // pages
+  remove_menu_page('tools.php'); // tools
+}
+add_action( 'admin_menu', 'devnull_remove_menu_pages' );
