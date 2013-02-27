65ed08b2e6c2ff88faec086fed35f02f8a045cc8
frebro <frebro@gmail.com>
2013-02-27 13:14:52 +0100

    removed link to helper.js

--- wordpress/wp-content/themes/devnull/footer.php
+++ wordpress/wp-content/themes/devnull/footer.php
@@ -1,6 +1,5 @@
     <script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery-1.9.1.min.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery.flexslider-min.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/js/helper.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
 
   </body>
