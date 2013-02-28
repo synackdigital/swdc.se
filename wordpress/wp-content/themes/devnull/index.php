a7eb0a1421e5e4f7a692218cec324f9ab1ba4fca
frebro <frebro@gmail.com>
2013-02-28 11:51:33 +0100

    static content cleanup

--- wordpress/wp-content/themes/devnull/index.php
+++ wordpress/wp-content/themes/devnull/index.php
@@ -26,53 +26,6 @@ get_header(); ?>
       <?php
       // End the Loop
       endwhile; endif; ?>
-
-      <aside class="flexslider carousel">
-        <ul class="slides">
-          <li>
-            <img src="http://www.escapedevnull.com/files/2012/02/Screenshot-at-2012-04-10-215535.png" alt="">
-          </li>
-          <li>
-            <img src="http://www.escapedevnull.com/files/2012/02/IMG_20121020_134106.jpg" alt="">
-          </li>
-        </ul>
-      </aside>
-
-      <aside>
-        <h1>Past events</h1>
-        <ul class="invisilist events">
-          <li class="box green event">
-            <img src="http://genericwitticism.com/spotify-logo-96x96-no-tagline.png" alt="" class="sponsor">
-            <h2><a href="#">/dev/null Spotify</a></h2>
-            <span class="poi">Debaser Medis, Stockholm</span>
-            <span class="datetime">24 April 2013, 08:30:00 CET</span>
-          </li>
-          <li class="box cyan event">
-            <img src="http://genericwitticism.com/devnull/dice.png" alt="" class="sponsor">
-            <h2><a href="#">/dev/null DICE 10</a></h2>
-            <span class="poi">Debase Medis, Stockholm</span>
-            <span class="datetime">24 April 2013, 08:30:00 CET</span>
-          </li>
-          <li class="box blue event">
-            <img src="http://www.greenergrasswebdesign.com/valtechlogo.png" alt="" class="sponsor">
-            <h2><a href="#">/dev/null Valtech</a></h2>
-            <span class="poi">Valtech, Stockholm</span>
-            <span class="datetime">24 April 2013, 08:30:00 CET</span>
-          </li>
-          <li class="box violet event">
-            <img src="http://genericwitticism.com/devnull/dice.png" alt="" class="sponsor">
-            <h2><a href="#">/dev/null DICE</a></h2>
-            <span class="poi">Debaser Medis, Stockholm</span>
-            <span class="datetime">24 April 2013, 08:30:00 CET</span>
-          </li>
-          <li class="box magenta event">
-            <img src="http://genericwitticism.com/svensklanemarknad.png" alt="" class="sponsor">
-            <h2><a href="#">Lost in Space with Svensk Lånemarknad</a></h2>
-            <span class="poi">Svensk Lånemarknad, Stockholm</span>
-            <span class="datetime">24 April 2013, 08:30:00 CET</span>
-          </li>
-        </ul>
-      </aside>
     </div>
 
 <?php get_footer(); ?>
