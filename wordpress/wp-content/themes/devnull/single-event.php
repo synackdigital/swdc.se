3a27801788b35551c0de1f3057ecd551b769b9b9
frebro <frebro@gmail.com>
2013-03-04 15:41:42 +0100

    single event templates and styling

--- wordpress/wp-content/themes/devnull/single-event.php
+++ wordpress/wp-content/themes/devnull/single-event.php
@@ -7,17 +7,20 @@
 get_header(); ?>
 
     <div id="main">
-      <?php
-      // Echo a link to home
-      if(!is_front_page())
-        echo '<a class="home-link" href="'.home_url().'" title="Return to home">Home</a>';
+      <a class="home-link" href="<?php echo home_url(); ?>" title="Return to home">Home</a>
 
+      <?php
       // Start the Loop
       if(have_posts()): while(have_posts()): the_post(); ?>
-      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+      <article id="post-<?php the_ID(); ?>" <?php post_class('eo-event'); ?>>
         <header class="entry-header">
+          <?php
+            if (has_post_thumbnail())
+              the_post_thumbnail();
+          ?>
           <h1 class="entry-title"><?php the_title(); ?></h1>
         </header>
+        <?php eo_get_template_part('event-meta','event-single'); ?>
         <div class="entry-content">
           <?php the_content(); ?>
         </div>
