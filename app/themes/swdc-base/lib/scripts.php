590d77cbd9bc199c51e5d6606c5fa5ef3869b1c7
frebro <frebro@gmail.com>
2013-10-15 08:49:34 +0200

    removed some bs scripts from compile

--- app/themes/swdc-base/lib/scripts.php
+++ app/themes/swdc-base/lib/scripts.php
@@ -27,7 +27,7 @@ function roots_scripts() {
   }
 
   wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.6.2.min.js', false, null, false);
-  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', false, '2a3e700c4c6e3d70a95b00241a845695', true);
+  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', false, '331306be1c8eaed5b58eeffde2ad212b', true);
   wp_enqueue_script('modernizr');
   wp_enqueue_script('jquery');
   wp_enqueue_script('roots_scripts');
