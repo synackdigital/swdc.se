fad6580ac2d9856fa5168cf7aa7f4fd3f4991a9c
frebro <frebro@gmail.com>
2014-10-22 10:04:33 +0200

    added multisite config to staging

--- config/environments/staging.php
+++ config/environments/staging.php
@@ -11,3 +11,10 @@ define('WP_SITEURL', getenv('WP_SITEURL'));
 ini_set('display_errors', 0);
 define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
+
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', false);
+define('DOMAIN_CURRENT_SITE', 'staging.swdc.se');
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
